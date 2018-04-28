a700e2437d85df97971ed60d84801b27c90fee5e
Sunny Ripert <sunny@sunfox.org>
2018-04-28 12:49:29 +0200

    Minor rspec configuration updates

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
 require "bundler/setup"
-require "all3dp"
-
 require "webmock/rspec"
+require "all3dp"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
@@ -11,10 +10,6 @@ RSpec.configure do |config|
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
-
-  config.expect_with :rspec do |c|
-    c.syntax = :expect
-  end
 
   config.before(:all) do
     WebMock.disable_net_connect!
