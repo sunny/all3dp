dda00d90c7571fe90efe7e90dd390272224039a9
Sunny Ripert <sunny@sunfox.org>
2018-04-28 12:05:18 +0200

    Add WebMock to specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 require "bundler/setup"
 require "all3dp"
 
+require "webmock/rspec"
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
@@ -10,5 +12,9 @@ RSpec.configure do |config|
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
+  end
+
+  config.before(:all) do
+    WebMock.disable_net_connect!
   end
 end
